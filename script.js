38cc6849270cee2e81a8c7eef58f208bdee9a364
Dani JM <xdanielmtz@gmail.com>
2021-03-01 14:52:49 -0500

    wrote a setinteral function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+var timeLeft = 5;
+
+prepareRead()
 
 function prepareRead() {
-  // Create the countdown timer.
+ var prepareInterval = setInterval(function () {
+    console.log("working");
+ },1000)
 }
 
 function speedRead() {
