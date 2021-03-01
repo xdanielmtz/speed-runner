793b07181c6c35d27d44e96b46ffea14f8470825
Dani JM <xdanielmtz@gmail.com>
2021-03-01 15:46:05 -0500

    added comments

--- script.js
+++ script.js
@@ -6,16 +6,25 @@ var timeEl = document.getElementById("timeEl");
 prepareRead()
 
 function prepareRead() {
+  //Everything in this function will execute every second
  var prepareInterval = setInterval(function () {
     console.log("working");
+    // the time will decrement every second
     timeLeft --;
+    // setting the HTML text to dynamically change with the seconds
     timeEl.textContent = timeLeft + " seconds until start."
     if(timeLeft === 0){
+      // cleared the interval when reaching 0
       clearInterval(prepareInterval);
+      // set it to empty string to have it appear nothing on screen
+      timeEl.textContent = "";
+      // starts the speedRead function
+      speedRead();
     }
  },1000)
 }
 
 function speedRead() {
   // Print words to the screen one at a time.
+  console.log("print the speed reader.")
 }
